3e103dbdf1ec41264a8d33f2bce3cd90af63c1b3
Alice Liddell <alice@example.com>
2026-01-11 14:49:38 +0530

    regular update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav">
-        <div className="nav-logo">Madhu Muddala</div>
+        <div className="nav-logo">Madhu Muddalaa</div>
         <ul className="nav-links">
           <li>
             <a href="#summary">Summary</a>
